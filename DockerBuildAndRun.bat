87dc04fa9d48b2965cfaa272b7bf5df8e93a4ec2
assane.diop <assane.diop@gmail.com>
2019-12-19 10:32:11 +0000

    Intrafiles - update volumes creation and removing docker compose file

--- DockerBuildAndRun.bat
+++ DockerBuildAndRun.bat
@@ -1,4 +1,4 @@
 Docker rm -f intrafiles
 Docker rmi intrafiles:latest
 Docker build -t intrafiles:latest .
-Docker run --name intrafiles -d -p 10000:8080 -p 10001:8081 intrafiles:latest
+Docker run --name intrafiles -d -v ca:/opt/ca -v partners:/opt/partners_content -p 10000:8080 -p 10001:8081 intrafiles:latest
